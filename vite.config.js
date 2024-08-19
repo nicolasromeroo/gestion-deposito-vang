42c5b0343030f853176985337cb0acc0b1b3e72f
Nicolas Romero <nicolasdev290300@gmail.com>
2024-08-19 20:27:41 -0300

    vite.congfig

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,24 @@
 //   plugins: [react()],
 // })
 
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       external: [
+//         'bootstrap/dist/css/bootstrap.min.css',
+//         'react-router-dom',
+//         'firebase/firestore',
+//         'firebase/app'
+//       ],
+//     },
+//   },
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -13,13 +31,8 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: [
-        'bootstrap/dist/css/bootstrap.min.css',
-        'react-router-dom',
-        'firebase/firestore',
-        'firebase/app'
-      ],
     },
   },
 });
+
 
